7354abb2d60f7ac8200f0a22e667a15580e6e3a3
chandu54321 <kasinenichandu@gmai.com>
2025-02-06 09:59:53 +0530

    ad

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.85.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
 resource "aws_vpc" "first" {
 cidr_block= "10.0.0.0/16"
 tags = {
